f6bc389a31999fa15bf63e9d8904f5d48021ea07
KarimHweidy97 <134971286+KarimHweidy97@users.noreply.github.com>
2023-08-30 18:40:10 +0300

    first commit

---
+++ introduction.h
@@ -0,0 +1,11 @@
+welcome to MEGA project one of Advanced diploma for ENG AMGAD SAMIR 
+
+this is the project of group G2 
+
+requriments : 
+making a digital clock / alarm using RTC unit, nucleo board  and blue pill board to achive theese requirments 
+1- log in system consist of ID and password and the user only have 3 incorrect log ins as maximum 
+2 welcome massage 
+3 menu : a- display time and date on lcd 
+         b- set time and date 
+		 c- set alarm : name and number 
